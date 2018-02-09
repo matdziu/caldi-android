6da0dc7831d3253464d40930d652aad4cab0b9f5
Mateusz Dziubek <matdziu@gmail.com>
2018-02-09 07:19:07 +0100

    Added splash layout as background

--- app/src/main/java/com/confi/splash/SplashActivity.kt
+++ app/src/main/java/com/confi/splash/SplashActivity.kt
@@ -4,15 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
-import com.confi.R
 import com.confi.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-
         Handler().postDelayed({
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
