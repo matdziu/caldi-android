0bfc4d2fb72e1df17e55b7e8c3502313c7cba05d
Mateusz Dziubek <matdziu@gmail.com>
2018-02-12 16:15:03 +0100

    Added mock sign up interactor

--- app/src/main/java/com/caldi/signup/SignUpInteractor.kt
+++ app/src/main/java/com/caldi/signup/SignUpInteractor.kt
@@ -1,11 +1,23 @@
 package com.caldi.signup
 
 import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 
 class SignUpInteractor {
 
     fun createAccount(email: String, password: String): Observable<PartialSignUpViewState> {
-        return Observable.just(PartialSignUpViewState.SignUpSuccess())
+        return if (email != "error") {
+            Observable.timer(3000, TimeUnit.MILLISECONDS).map { PartialSignUpViewState.SignUpSuccess() }
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeOn(Schedulers.io()) as Observable<PartialSignUpViewState>
+        } else {
+            Observable.timer(100, TimeUnit.MILLISECONDS).map { PartialSignUpViewState.ErrorState(true) }
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeOn(Schedulers.io())
+                    .startWith(PartialSignUpViewState.ErrorState()) as Observable<PartialSignUpViewState>
+        }
     }
 }
