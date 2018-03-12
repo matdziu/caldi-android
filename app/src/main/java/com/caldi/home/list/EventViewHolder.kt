3af0bb2e64d17c3a246ac3389a0aa9e5600680bc
Mateusz Dziubek <matdziu@gmail.com>
2018-03-12 09:26:27 +0100

    Added placeholder for event item

--- app/src/main/java/com/caldi/home/list/EventViewHolder.kt
+++ app/src/main/java/com/caldi/home/list/EventViewHolder.kt
@@ -2,6 +2,7 @@ package com.caldi.home.list
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import com.caldi.R
 import com.caldi.eventprofile.EventProfileActivity
 import com.caldi.home.models.Event
 import com.squareup.picasso.Picasso
@@ -13,7 +14,10 @@ class EventViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun bindEvent(event: Event) {
         with(itemView) {
             eventTextView.text = event.name
-            Picasso.get().load(event.imageUrl).into(eventImageView)
+            Picasso.get()
+                    .load(event.imageUrl)
+                    .placeholder(R.drawable.event_placeholder)
+                    .into(eventImageView)
             setOnClickListener { EventProfileActivity.start(context, event.id) }
         }
     }
