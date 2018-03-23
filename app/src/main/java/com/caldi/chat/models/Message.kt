49a982737ad8cdffb9ba7f3481a749e4c6961c78
Mateusz Dziubek <matdziu@gmail.com>
2018-03-23 17:00:13 +0100

    Now pushing not empty messages to onNewMessageAdded

--- app/src/main/java/com/caldi/chat/models/Message.kt
+++ app/src/main/java/com/caldi/chat/models/Message.kt
@@ -1,3 +1,10 @@
 package com.caldi.chat.models
 
-data class Message(val timestamp: Long = 0, val message: String = "", val senderId: String = "")
+data class Message(val timestamp: Long = 0, val message: String = "", val senderId: String = "") {
+
+    fun isNotEmpty(): Boolean {
+        return timestamp != 0L
+                && message.isNotEmpty()
+                && senderId.isNotEmpty()
+    }
+}
