41f0c814b4e3576f72e0154f3690c3ebce0c11a3
Mateusz Dziubek <matdziu@gmail.com>
2018-03-29 18:11:48 +0200

    Now OrganizerActivity can show progress bar

--- app/src/main/java/com/caldi/organizer/OrganizerActivity.kt
+++ app/src/main/java/com/caldi/organizer/OrganizerActivity.kt
@@ -2,12 +2,16 @@ package com.caldi.organizer
 
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.view.View
 import com.caldi.R
 import com.caldi.base.BaseDrawerActivity
 import com.caldi.factories.OrganizerViewModelFactory
 import dagger.android.AndroidInjection
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
+import kotlinx.android.synthetic.main.activity_organizer.messagesRecyclerView
+import kotlinx.android.synthetic.main.activity_organizer.organizerInfoView
+import kotlinx.android.synthetic.main.activity_organizer.progressBar
 import javax.inject.Inject
 
 class OrganizerActivity : BaseDrawerActivity(), OrganizerView {
@@ -42,5 +46,17 @@ class OrganizerActivity : BaseDrawerActivity(), OrganizerView {
 
     override fun render(organizerViewState: OrganizerViewState) {
 
+    }
+
+    private fun showProgressBar(show: Boolean) {
+        progressBar.visibility = View.GONE
+        if (show) {
+            progressBar.visibility = View.VISIBLE
+            messagesRecyclerView.visibility = View.GONE
+            organizerInfoView.visibility = View.GONE
+        } else {
+            messagesRecyclerView.visibility = View.VISIBLE
+            organizerInfoView.visibility = View.VISIBLE
+        }
     }
 }
--- app/src/main/java/com/caldi/organizer/OrganizerViewState.kt
+++ app/src/main/java/com/caldi/organizer/OrganizerViewState.kt
@@ -3,5 +3,6 @@ package com.caldi.organizer
 import com.caldi.organizer.models.Message
 
 data class OrganizerViewState(val messagesList: List<Message> = listOf(),
+                              val progress: Boolean = false,
                               val error: Boolean = false,
                               val dismissToast: Boolean = false)
