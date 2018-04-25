d1d448ff748ff52652a2dd81b00d936eff290cec
Mateusz Dziubek <matdziu@gmail.com>
2018-04-25 11:09:24 +0200

    Integrated Firebase Crashlytics

--- app/src/main/java/com/caldi/CaldiApplication.kt
+++ app/src/main/java/com/caldi/CaldiApplication.kt
@@ -3,9 +3,11 @@ package com.caldi
 import android.app.Activity
 import android.app.Application
 import com.caldi.injection.DaggerAppComponent
+import com.crashlytics.android.Crashlytics
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
+import io.fabric.sdk.android.Fabric
 import javax.inject.Inject
 
 
@@ -16,6 +18,11 @@ class CaldiApplication : Application(), HasActivityInjector {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (!BuildConfig.DEBUG) {
+            Fabric.with(this, Crashlytics())
+        }
+
         DaggerAppComponent.builder()
                 .application(this)
                 .build()
