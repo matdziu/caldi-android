f59f9b1f882436b7b0a322cf7c3c743be1d8b5e8
Mateusz Dziubek <matdziu@gmail.com>
2018-05-10 22:43:45 +0200

    Now notifications receiver is compatible with cloud function

--- app/src/main/java/com/caldi/constants/NotificationConstants.kt
+++ app/src/main/java/com/caldi/constants/NotificationConstants.kt
@@ -3,6 +3,11 @@ package com.caldi.constants
 const val NOTIFICATION_TOKEN_KEY = "notificationTokenKey"
 const val NOTIFICATION_TOKEN_ACTION = "notificationTokenAction"
 
+const val NOTIFICATION_TYPE_KEY = "notification_type"
+const val NOTIFICATION_TITLE_LOC_ARGS = "title_loc_args"
+const val NOTIFICATION_BODY_LOC_ARGS = "body_loc_args"
+
 const val ORGANIZER_NOTIFICATION_REQUEST_CODE = 10
 const val ORGANIZER_NOTIFICATION_ID = 11
 const val ORGANIZER_CHANNEL_ID = "organizer"
+const val ORGANIZER_NOTIFICATION_TYPE = "organizer"
--- app/src/main/java/com/caldi/notifications/services/MessagingService.kt
+++ app/src/main/java/com/caldi/notifications/services/MessagingService.kt
@@ -8,33 +8,45 @@ import android.content.Intent
 import android.support.v4.app.NotificationCompat
 import android.support.v4.content.ContextCompat
 import com.caldi.R
+import com.caldi.constants.NOTIFICATION_BODY_LOC_ARGS
+import com.caldi.constants.NOTIFICATION_TITLE_LOC_ARGS
+import com.caldi.constants.NOTIFICATION_TYPE_KEY
 import com.caldi.constants.ORGANIZER_CHANNEL_ID
 import com.caldi.constants.ORGANIZER_NOTIFICATION_ID
 import com.caldi.constants.ORGANIZER_NOTIFICATION_REQUEST_CODE
+import com.caldi.constants.ORGANIZER_NOTIFICATION_TYPE
 import com.caldi.splash.SplashActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import org.json.JSONArray
 
 
 class MessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
-        val title = remoteMessage.data["title"]
-        val body = remoteMessage.data["body"]
-        showNotification(title, body)
+        val titleLocArgs = jsonStringToArray(remoteMessage.data[NOTIFICATION_TITLE_LOC_ARGS])
+        val bodyLocArgs = jsonStringToArray(remoteMessage.data[NOTIFICATION_BODY_LOC_ARGS])
+        val notificationType = remoteMessage.data[NOTIFICATION_TYPE_KEY]
+
+        when (notificationType) {
+            ORGANIZER_NOTIFICATION_TYPE -> handleOrganizerNotification(titleLocArgs, bodyLocArgs)
+        }
     }
 
-    private fun showNotification(title: String?, body: String?) {
+    private fun handleOrganizerNotification(titleLocArgs: Array<String>, bodyLocArgs: Array<String>) {
         val intent = Intent(this, SplashActivity::class.java)
         val pendingIntent = PendingIntent.getActivity(this, ORGANIZER_NOTIFICATION_REQUEST_CODE,
                 intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
+        val title = getString(R.string.organizer_message_notification_title, *titleLocArgs)
+        val body = getString(R.string.organizer_message_notification_body, *bodyLocArgs)
+
         val notification = NotificationCompat.Builder(this, ORGANIZER_CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_notification)
-                .setColor(ContextCompat.getColor(this, R.color.colorPrimary))
                 .setContentTitle(title)
                 .setContentText(body)
+                .setColor(ContextCompat.getColor(this, R.color.colorPrimary))
                 .setDefaults(Notification.DEFAULT_ALL)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setContentIntent(pendingIntent)
@@ -43,5 +55,14 @@ class MessagingService : FirebaseMessagingService() {
 
         val notificationsManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationsManager.notify(ORGANIZER_NOTIFICATION_ID, notification)
+    }
+
+    private fun jsonStringToArray(jsonString: String?): Array<String> {
+        val stringArray = arrayListOf<String>()
+        val jsonArray = JSONArray(jsonString)
+        for (index in 0 until jsonArray.length()) {
+            stringArray.add(jsonArray.getString(index))
+        }
+        return stringArray.toTypedArray()
     }
 }
