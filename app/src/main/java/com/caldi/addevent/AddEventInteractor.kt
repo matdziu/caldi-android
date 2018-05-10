9d54173b1d46a66a5c2978c38057a2ddf052e5db
Mateusz Dziubek <matdziu@gmail.com>
2018-05-10 11:27:32 +0200

    Now subscribing users to organizer messages topic

--- app/src/main/java/com/caldi/addevent/AddEventInteractor.kt
+++ app/src/main/java/com/caldi/addevent/AddEventInteractor.kt
@@ -10,6 +10,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.messaging.FirebaseMessaging
 import io.reactivex.Observable
 import io.reactivex.functions.BiFunction
 import io.reactivex.subjects.PublishSubject
@@ -82,6 +83,7 @@ class AddEventInteractor {
 
     private fun saveUserIdToEvent(userId: String?, eventId: String): Observable<Boolean> {
         val successSubject = PublishSubject.create<Boolean>()
+        subscribeUserToOrganizerMessages(eventId)
         eventsNodeRef
                 .child(eventId)
                 .child(ATTENDEES_NODE)
@@ -89,6 +91,10 @@ class AddEventInteractor {
                 .setValue(userId)
                 .addOnCompleteListener { successSubject.onNext(true) }
         return successSubject
+    }
+
+    private fun subscribeUserToOrganizerMessages(eventId: String) {
+        FirebaseMessaging.getInstance().subscribeToTopic(eventId)
     }
 
     private fun emitError(stateSubject: Subject<PartialAddEventViewState>) {
